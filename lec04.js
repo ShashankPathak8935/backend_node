00f9cd4804d317b80e1ae7d97585dd67c082eeb5
ShashankPathak8935 <pathakshashank285@gmail.com>
2025-09-18 22:57:10 +0530

    something about git

---
+++ lec04.js
@@ -0,0 +1,4 @@
+// lec 4
+// moon modular is a data modeling tool for mongo db and noSQL
+// data modeling means data ka pura structure define krna
+// jaise ki ek student ka data hoga to usme name, age, roll no, address, phone no etc hoga
---
+++ lec06.js
@@ -0,0 +1,33 @@
+// git branch -m main  --> isse existing branch ka name change kr skte hain
+// git checkout -b newBranch  --> new branch create krke usme chla jata h
+// git checkout branch ka name(main)  --> main branch pr chla jata h/ jis branch pr jane chahe
+// git merge new branch ka name(newBranch)  --> newBranch ko main me merge kr deta h
+// git branch -d branch ka name(newBranch)  --> newBranch ko delete kr deta h
+// git push origin main  --> changes ko remote repo me push kr deta h
+// git status --> current branch me kya changes hain wo batata h
+// git remote -v  --> remote repo ka url batata h
+// git log  --> commit history batata h
+// git reset --hard HEAD~1  --> last commit ko hata deta h aur changes ko bhi hata deta h
+// git reset --soft HEAD~1  --> last commit ko hata deta h lekin changes ko nahi hatata h
+// git stash  --> current changes ko temporary save kr deta h bina commit kiye
+// git stash pop  --> last stash ko wapas le aata h aur changes ko wapas la deta h
+// git fetch  --> remote repo se latest changes ko fetch kr leta h lekin local repo me nahi lata h
+// git pull  --> remote repo se latest changes ko fetch kr leta h aur local repo me bhi le aata h
+// git clone repo ka url  --> remote repo ko local machine me copy kr leta h
+// git init  --> current folder ko git repo me convert kr deta h
+// git add .  --> current folder ke sare changes ko staging area me daal deta h
+// git commit -m "message"  --> staging area ke changes ko commit kr deta h with message
+// git remote add origin repo ka url  --> local repo ko remote repo se connect kr deta h
+// git config --global user.name "name"  --> git me apna name set kr deta h
+// git config --global user.email "email"  --> git me apna email set kr deta h
+// git ignore file  --> is file me jo bhi files ya folders likhe honge wo git ignore kr dega
+// git ignore generators --> https://www.toptal.com/developers/gitignore
+// git diff  --> current changes aur last commit ke changes me kya difference h wo batata h
+// git commit --amend  --> last commit ko edit kr deta h
+// gitignore generator --> jis type ka project hai uske hisab se git ignore file generate krke de deta hai
+
+// dev dependency --> jo dependency sirf development ke time pe chahiye hoti hai jaise ki nodemon, eslint etc
+// npm install package name --save-dev  --> package ko dev dependency ke roop me install krta h
+// npm uninstall package name --save-dev  --> package ko dev dependency se uninstall krta h
+// npm i -D nodemon  --> nodemon ko dev dependency ke roop me install krta h
+
